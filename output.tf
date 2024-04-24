8fcac88690c3698b7844c02b8e7968e1f030343e
mcjara <moises@trustportal.org>
2024-04-24 11:55:12 +0100

    Adding lb_arn as an output

--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
-output "lb_http_443_listener" {
-  value = aws_lb_listener.http-443
+output "lb_arn" {
+  value = aws_lb.alb.arn
 }
 
 output "lb_dns_record" {
   value = aws_lb.alb.dns_name
+}
+
+output "lb_http_443_listener" {
+  value = aws_lb_listener.http-443
 }
